3091cf0f7546c01b1428d8c6d923a52e8d838ca0
sagar junnarkar <sagarjunnu@gmail.com>
2015-06-14 18:54:17 +0530

    best in place gem added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,10 @@
 //
 //= require jquery
 //= require jquery_ujs
+//= require best_in_place
 //= require turbolinks
 //= require_tree .
+$(document).ready(function() {
+  /* Activating Best In Place */
+  jQuery(".best_in_place").best_in_place();
+});
